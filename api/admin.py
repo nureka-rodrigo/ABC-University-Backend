098a43daff2906fae1142ab8d23f56ab8b3be0c6
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2023-12-02 00:03:28 +0530

    Configured admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+admin.site.register(models.User)
+admin.site.register(models.Student)
+admin.site.register(models.Lecturer)
+admin.site.register(models.Course)
+admin.site.register(models.Degree)
+admin.site.register(models.Department)
+admin.site.register(models.Faculty)
+admin.site.register(models.Result)
+admin.site.register(models.Feedback)
+
