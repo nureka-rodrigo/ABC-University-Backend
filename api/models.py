10664da6ffdc5a5d00f323ead306f55647035fa0
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2023-12-02 00:03:06 +0530

    Created main models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,100 @@
 from django.db import models
 
-# Create your models here.
+
+# User modal
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    role = models.CharField(max_length=20)
+
+    class Meta:
+        verbose_name = "01. User"
+
+
+# Lecturer modal
+class Lecturer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    tel = models.CharField(max_length=20)
+
+    class Meta:
+        verbose_name = "02. Lecturer"
+
+
+# Student modal
+class Student(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    tel = models.CharField(max_length=20)
+    dob = models.DateField(null=True)
+    description = models.TextField(null=True)
+
+    class Meta:
+        verbose_name = "03. Student"
+
+
+# Course modal
+class Course(models.Model):
+    code = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    credits = models.CharField(max_length=10)
+    type = models.CharField(max_length=10)
+
+    class Meta:
+        verbose_name = "04. Course"
+
+
+# Degree modal
+class Degree(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = "05. Degree"
+
+
+# Department modal
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = "06. Department"
+
+
+# Faculty modal
+class Faculty(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = "07. Facultie"
+
+
+# Semester modal
+class Semester(models.Model):
+    name = models.CharField(max_length=20)
+
+    class Meta:
+        verbose_name = "08. Semester"
+
+
+# Result modal
+class Result(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    grade = models.CharField(max_length=10)
+
+    class Meta:
+        verbose_name = "09. Result"
+
+
+# Feedback modal
+class Feedback(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    question = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = "10. Feedback"
