711a00e956854e0374e95aaaf45b0e678dc0eb18
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2023-12-03 02:01:25 +0530

    Created api endpoints to set and delete cookies

--- api/urls.py
+++ api/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('register/', views.create_user, name='register'),
     path('logout/', knox.views.LogoutView.as_view(), name='logout'),
     path('logoutall/', knox.views.LogoutAllView.as_view(), name='logout all'),
-    path('cookieset/', views.set_cookie, name='set cookie'),
-    path('cookiedelete/', views.delete_cookie, name='delete cookie'),
+    path('set_cookie/', views.set_cookie, name='set cookie'),
+    path('delete_cookie/', views.delete_cookie, name='delete cookie'),
 ]
