a59c5ea593783ad5afa50d173e595515c26c8335
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2023-12-03 22:17:14 +0530

    Updated the project

--- api/views.py
+++ api/views.py
@@ -5,7 +5,6 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-
 from . import models
 from .serializers import UserSerializer
 
